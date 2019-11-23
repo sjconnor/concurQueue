daf68a3c60109d9e573a80aa27642e058d589536
Sarah Connor <sarah@tinfoilwizard.net>
2019-11-23 13:41:32 -0800

    updated make, started writing tests

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
+.PHONY: all test clean
+
 all: main.c concurQueue.c 
 	gcc -g -Wall -o cq concurQueue.c main.c
+
+test: all
+	valgrind ./cq
 
 clean: 
 	$(RM) cq
--- main.c
+++ main.c
@@ -21,48 +21,10 @@ int main() {
     printf("You can do it!\n");
 
     // for testing
-    int (*Queue_matchFn)(void*, void*) = &matchFn;
+    Queue_matchFn comparator = &matchFn;
 
     // build a queue
     struct Queue *myQ = Queue_new(5);
-
-    // some dumb data for now
-    int a, b, c, d, e;
-    a = 1;
-    b = 2;
-    c = 3;
-    d = 4;
-    e = 5;
-    void *ptr1 = &a;
-    void *ptr2 = &b;
-    void *ptr3 = &c;
-    void *ptr4 = &d;
-    void *ptr5 = &e;
-
-    Queue_add(myQ, ptr1);
-    Queue_add(myQ, ptr2);
-    Queue_add(myQ, ptr3);
-    Queue_add(myQ, ptr4);
-    Queue_add(myQ, ptr5);
-    //Queue_add(myQ, ptr5); // this should error
-    //Queue_add(myQ, ptr5);
-    //Queue_add(myQ, ptr5);
-
-    Queue_find(myQ, Queue_matchFn, ptr5);
-
-    Queue_remove(myQ);
-    Queue_remove(myQ);
-    //Queue_remove(myQ);
-    //Queue_remove(myQ);
-    //Queue_remove(myQ);
-
-    Queue_add(myQ, ptr1);
-    Queue_add(myQ, ptr2);
-    Queue_add(myQ, ptr2);
-
-    Queue_remove(myQ);
-    Queue_remove(myQ);
-    Queue_remove(myQ);
 
 
     printf("front val = %i, idx = %lu\n", *((int*)myQ->queue[myQ->front]), myQ->front);
